3eb9c584b0af489a3def42e2d8b05a301b2515eb
Kimberly Wilber <kimmy@kjwilber.org>
2024-05-01 18:19:16 -0400

    Fix package structure

--- src/kdb.nim
+++ src/kdb.nim
@@ -1,10 +1,26 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
-import kdb/essentials/docs
-import kdb/libraries/sqliteLibrary
-import cligen
+import kdbpkg/essentials/[docs, parsing, vocabulary, uuid, repr]
+import kdbpkg/libraries/sqliteLibrary
+import std/[strutils]
+
+proc p(exprs: varargs[string]) =
+  var lib = openSqliteLibrary()
+  var vocab = lib.getSchema()
+  var doc = makeDoc(uuid())
+  var parser = parsing.ParseState(
+    input: expr.join(" "), vocab: vocab)
+  parser.parse()
+  if parser.kind == parseOk:
+    doc.children = parser.results
+    echo doc.reprFull
+
+  else:
+    raise newException(ValueError, "Could not parse: " & $parser)
 
 when isMainModule:
-  var u = openSqliteLibrary()
-  u.add: newDoc "abc-def": title "Foobar"
+  import cligen
+  cligen.dispatchMulti(
+    [p], # [add]
+  )
