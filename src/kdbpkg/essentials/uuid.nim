1f1e4e2b24148926d7377f9677317bc67b623740
Kimberly Wilber <kimmy@kjwilber.org>
2024-05-04 09:08:57 -0400

    UUIDs are now just five letters

--- src/kdbpkg/essentials/uuid.nim
+++ src/kdbpkg/essentials/uuid.nim
@@ -1,22 +1,39 @@
 import random
 import strformat
+import strutils
+import sequtils
 import docs
+import sugar
 randomize()
 
 ## Implementation of proquints.
 
 let
     consonants = "bdfghjklmnprstvz" ## 16 bits
-    vowels = "aiou" ## 2 bits
+    vowels = "aiou"                 ## 2 bits
 
-proc con():string = $consonants[rand(max=consonants.high.int)]
-proc vow():string = $vowels[rand(max=vowels.high.int)]
+    flickr_base88 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+    ## about 5 bits
+
+proc con(): string = $consonants[rand(max = consonants.high.int)]
+proc vow(): string = $vowels[rand(max = vowels.high.int)]
 
 
 proc proquint(): string =
     fmt"{con()}{vow()}{con()}{vow()}{con()}"
+
+proc randomLetters(len=5): string =
+    let letters = collect:
+        for _ in 0..<len:
+            flickr_base88[rand(max=flickr_base88.len-1)]
+    result = ":" & letters.join()
+
+
+
+
 proc uuid*(): ID =
-    toID(fmt":{proquint()}-{proquint()}")
+    #toID(fmt":{proquint()}-{proquint()}")
+    toID(randomLetters(5))
 
 when isMainModule:
     echo uuid()
